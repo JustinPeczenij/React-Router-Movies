854d39f623759cbf5d18e6ee6bb49408d852daf1
Justin Peczenij <justinpeczenij@gmail.com>
2021-03-22 19:32:23 -0400

    added functionality to home button and movies

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,28 +1,33 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
 
 export default function MovieList(props) {
-  console.log(props)
+
+  const { url } = useRouteMatch();
+  console.log(url)
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
+        <Link key={movie.id}to={`${url}movies/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
+        ))}
     </div>
   );
 }
 
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
-
+  
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
       </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
   );
 }
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useRouteMatch, useHistory } from 'react-router-dom';
 
 export default function SavedList(props) {
+  
+  // add functionality to the Home Div
+  const history = useHistory();
+  const routeHome = () => {
+    console.log(history)
+    history.push(`/`)
+  }
+
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">Home</div>
+      <div onClick={routeHome} className="home-button">Home</div>
     </div>
   );
 }
